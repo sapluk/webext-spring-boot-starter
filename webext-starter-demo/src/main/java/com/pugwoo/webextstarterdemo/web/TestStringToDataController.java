e65f4e825fe329c10e28fb62f9d97dcac71bb264
pugwoo <pugwoo@gmail.com>
2018-09-17 09:00:39 +0800

    发现问题

--- webext-starter-demo/src/main/java/com/pugwoo/webextstarterdemo/web/TestStringToDataController.java
+++ webext-starter-demo/src/main/java/com/pugwoo/webextstarterdemo/web/TestStringToDataController.java
@@ -2,6 +2,7 @@ package com.pugwoo.webextstarterdemo.web;
 
 import com.pugwoo.webextstarterdemo.bean.ErrorCode;
 import com.pugwoo.webextstarterdemo.bean.WebJsonBean;
+import com.pugwoo.webextstarterdemo.web.form.MyForm;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +16,34 @@ import java.util.Date;
 @RestController
 public class TestStringToDataController {
 
+    /**
+     * http://127.0.0.1:8080/testStringToData?time=20180808
+     * @param time
+     * @return
+     */
     @RequestMapping("/testStringToData")
     public WebJsonBean testStringToData(Date time){
         if (time != null) {
             return new WebJsonBean(time);
         }
         return new WebJsonBean(ErrorCode.MISSING_PARAMETERS,"time参数未提供或者格式错误，请确保time参数正确提供，如 ?time=20180808");
+    }
+
+    /**
+     * http://127.0.0.1:8080/testStringToDate2?myDate=20180808
+     *
+     * 发现问题：对于对象的形式，String没有转成Date
+     * @param form
+     * @return
+     */
+    @RequestMapping("/testStringToDate2")
+    public WebJsonBean testStringDate2(MyForm form) {
+        if(form == null) {
+            return new WebJsonBean(ErrorCode.COMMON_BIZ_ERROR, "form is null");
+        }
+        if(form.getMyDate() == null) {
+            return new WebJsonBean(ErrorCode.COMMON_BIZ_ERROR, "form.myDate is null");
+        }
+        return new WebJsonBean(form.getMyDate());
     }
 }

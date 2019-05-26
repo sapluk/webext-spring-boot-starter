7064e25fcab4abc190ab135e6d7c18bd71563c34
pugwoo <pugwoo@gmail.com>
2019-05-26 08:16:04 +0800

    release 0.0.6

--- webext-starter-demo/src/main/java/com/pugwoo/webextstarterdemo/web/form/MyForm.java
+++ webext-starter-demo/src/main/java/com/pugwoo/webextstarterdemo/web/form/MyForm.java
@@ -1,9 +1,12 @@
 package com.pugwoo.webextstarterdemo.web.form;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.Date;
 
 public class MyForm {
 
+    // @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date myDate;
 
     public Date getMyDate() {

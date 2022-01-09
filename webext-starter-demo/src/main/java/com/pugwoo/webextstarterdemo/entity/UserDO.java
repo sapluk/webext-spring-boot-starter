0c6ff7342dfa9926312c591291889f7453e85d7f
sapluk <yeah16@qq.com>
2022-01-09 14:19:06 +0800

    test @JsonParam支持@Validated进行数据校验

--- webext-starter-demo/src/main/java/com/pugwoo/webextstarterdemo/entity/UserDO.java
+++ webext-starter-demo/src/main/java/com/pugwoo/webextstarterdemo/entity/UserDO.java
@@ -2,6 +2,9 @@ package com.pugwoo.webextstarterdemo.entity;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 /**
  * 演示用<br/>
  *  此处使用 lombok 插件，自动生成getter/setter等<br/>
@@ -10,9 +13,11 @@ import lombok.Data;
  */
 @Data
 public class UserDO {
-
+    
+    @NotNull
     private String name;
 
+    @NotBlank
     private String password;
 
     private Integer age;
--- webext-starter-demo/src/main/java/com/pugwoo/webextstarterdemo/web/TestJsonParamController.java
+++ webext-starter-demo/src/main/java/com/pugwoo/webextstarterdemo/web/TestJsonParamController.java
@@ -1,13 +1,16 @@
 package com.pugwoo.webextstarterdemo.web;
 
 import com.pugwoo.bootwebext.JsonParam;
-import com.pugwoo.webextstarterdemo.bean.ErrorCode;
 import com.pugwoo.webextstarterdemo.bean.WebJsonBean;
 import com.pugwoo.webextstarterdemo.entity.UserDO;
-import com.pugwoo.webextstarterdemo.service.IUserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * 测试 @JsonParam 自动解析json字符串为参数 <br/>
@@ -16,19 +19,68 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestJsonParamController {
+    
+    private final static Logger LOGGER = LoggerFactory.getLogger(TestJsonParamController.class);
 
-    @Autowired
-    private IUserService userService;
-
-    /** 测试JsonParam
-     *      /testJsonParam?test={"name":"abc","password":123456,"age":16}
-     *      上述请求需进行转码，响应转码为:
-     *  http://127.0.0.1:8080/testJsonParam?test=%7b%22name%22%3a%22abc%22%2c%22password%22%3a123456%2c%22age%22%3a16%7d
+    /**
+     * 测试 @JsonParam，指定了value值，可采用url, x-www-form-urlencoded, multipart/form-data
+     * 测试见 http.testJsonParam.http
      */
     @RequestMapping("/testJsonParam")
     public WebJsonBean testJsonParam(@JsonParam("test") UserDO userDO){
-        boolean success = userService.insertOrUpdate(userDO);
-        return success ? new WebJsonBean(userDO) : new WebJsonBean(ErrorCode.COMMON_BIZ_ERROR);
+        LOGGER.debug("testJsonParam: {}", userDO);
+        return new WebJsonBean(userDO);
     }
-
+    
+    /**
+     * 测试 @JsonParam，未指定value值，可采用json
+     * 测试见 http.testJsonParam.http
+     */
+    @RequestMapping("/testJsonParamWithoutValue")
+    public WebJsonBean testJsonParamWithoutValue(@JsonParam UserDO userDO){
+        LOGGER.debug("testJsonParamWithoutValue: {}", userDO);
+        return new WebJsonBean(userDO);
+    }
+    
+    // ---------------------------------------------------------------------------
+    
+    /**
+     * 测试 @Valid @JsonParam
+     * 测试见 http.testJsonParamValid.http
+     */
+    @RequestMapping("/testValidJsonParam")
+    public WebJsonBean testValidJsonParam(@Valid @JsonParam UserDO userDO){
+        LOGGER.debug("testValidJsonParam: {}", userDO);
+        return new WebJsonBean(userDO);
+    }
+    
+    /**
+     * 测试 @Validated @JsonParam
+     * 测试见 http.testJsonParamValid.http
+     */
+    @RequestMapping("/testValidatedJsonParam")
+    public WebJsonBean testValidatedJsonParam(@Validated @JsonParam UserDO userDO){
+        LOGGER.debug("testValidatedJsonParam: {}", userDO);
+        return new WebJsonBean(userDO);
+    }
+    
+    /**
+     * 测试 @Valid @RequestBody 主要用于对比@JsonParam
+     * 测试见 http.testJsonParamValid.http
+     */
+    @RequestMapping("/testValidRequestBody")
+    public WebJsonBean testValidRequestBody(@Valid @RequestBody UserDO userDO){
+        LOGGER.debug("testValidRequestBody: {}", userDO);
+        return new WebJsonBean(userDO);
+    }
+    
+    /**
+     * 测试 @Validated @RequestBody 主要用于对比@JsonParam
+     * 测试见 http.testJsonParamValid.http
+     */
+    @RequestMapping("/testValidatedRequestBody")
+    public WebJsonBean testValidatedRequestBody(@Validated @RequestBody UserDO userDO){
+        LOGGER.debug("testValidatedRequestBody: {}", userDO);
+        return new WebJsonBean(userDO);
+    }
 }
